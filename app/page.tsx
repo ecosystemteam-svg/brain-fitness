62ee0ab01c029de8a4bdc509c7bf1d23670ecfe6
ecosystemteam-svg <ecosystemteam@eisaith.com>
2026-02-27 12:59:03 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
+"use client";
 
+import dynamic from "next/dynamic";
+
+const BrainFitness = dynamic(
+  () => import("../components/BrainFitness"),
+  { ssr: false }
+);
+
+export default function Home() {
+  return <BrainFitness />;
+}
